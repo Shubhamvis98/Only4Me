97c2afd53bdbc40f853521051092f5dbea175634
Shubham Vishwakarma <43336210+Shubhamvis98@users.noreply.github.com>
2024-08-16 01:25:31 +0530

    Update kernel_builder.sh

--- kernel_builder.sh
+++ kernel_builder.sh
@@ -6,7 +6,17 @@ FROM debian:latest
 ENV DEBIAN_FRONTEND=noninteractive
 
 RUN apt-get update && \
-    apt-get install -y zip unzip python3 python-is-python3 curl wget git rsync bison bc flex file vim pkgconf debhelper-compat kmod cpio libssl-dev libelf-dev build-essential crossbuild-essential-arm64 libncurses-dev && \
+    apt-get install -y \
+    zip unzip python3 \
+    python-is-python3 \
+    curl wget git rsync \
+    bison bc flex file \
+    vim pkgconf cpio \
+    debhelper-compat \
+    kmod libssl-dev \
+    libelf-dev build-essential \
+    crossbuild-essential-arm64 \
+    libncurses-dev && \
     apt-get clean
 RUN echo 'echo -e "Kernel Builder by fossfrog\nTwitter: ShubhamVis98\nWeb: https://fossfrog.in\n"' >> /root/.bashrc
 RUN echo 'echo -e "Kernel Builder by fossfrog\nTwitter: ShubhamVis98\nWeb: https://fossfrog.in\n"' > /etc/motd
