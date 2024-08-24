26a7718c180d71c0b26451d4374282b3550af057
Shubham Vishwakarma <43336210+Shubhamvis98@users.noreply.github.com>
2024-08-24 12:23:59 +0530

    update

--- linux/losetup_emmc_img.sh
+++ linux/losetup_emmc_img.sh
@@ -1,46 +1,48 @@
 #!/bin/bash
 
 RAMDISK=ramdisk/scripts/local-premount
-SCRIPT=$RAMDISK/losetup_root_img
+SCRIPT=${RAMDISK}/losetup_root_img
 
 
-[ ! -d "$RAMDISK" ] && echo '[!] Ramdisk extract not found' && exit
+[ ! -d "${RAMDISK}" ] && echo '[!] Ramdisk extract not found' && exit
 
-cat <<'eof'> $SCRIPT
+cat <<'eof'> ${SCRIPT}
 #!/bin/sh
 
 set -e
 
 case "${1}" in
-        prereqs)
-                exit 0
-                ;;
+	prereqs)
+		exit 0
+		;;
 esac
 
 for x in $(cat /proc/cmdline); do
-    case $x in
-        rootimg=*)
-            ROOTIMG=/userdata${x#rootimg=}
-            ;;
-    esac
+	case $x in
+		rootimg=*)
+			ROOTIMG=${x#rootimg=}
+			IMGPART=`echo ${ROOTIMG} | cut -d'/' -f2`
+			;;
+	esac
 done
 
 mkdir /userdata
-mount `blkid -t PARTLABEL=userdata -o device` /userdata || exit 0
+mount `blkid -t PARTLABEL=${IMGPART} -o device` /userdata || exit 0
 
-if [ -e "$ROOTIMG" ]
+if [ -e "${ROOTIMG}" ]
 then
-    losetup -f $ROOTIMG
+	losetup -f ${ROOTIMG}
 else
-    umount /userdata
-    rmdir /userdata
+	umount /userdata
+	rmdir /userdata
 fi
 
 eof
 
-chmod 755 $SCRIPT
-if ! grep -q losetup_root_img $RAMDISK/ORDER
+chmod 755 ${SCRIPT}
+if ! grep -q losetup_root_img ${RAMDISK}/ORDER
 then
 	sed -i '1i/scripts/local-premount/losetup_root_img "$@"\n[ -e /conf/param.conf ] && . /conf/param.conf' \
-		$RAMDISK/ORDER
+		${RAMDISK}/ORDER
 fi
+
